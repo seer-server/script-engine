caf8e0fdf91e484929c83c74d8ee9f5ea22c43c0
Brandon Buck <lordizuriel@gmail.com>
2015-04-25 23:05:59 -0500

    Add some documentation.

--- engine.go
+++ engine.go
@@ -1,49 +1,55 @@
+// Copyright (c) 2015 tree-server contributors
+
 package engine
 
 import "github.com/yuin/gopher-lua"
 
+// Engine struct stores a pointer to a lua.LState providing a simplified API.
 type Engine struct {
 	state *lua.LState
 }
 
+// ScriptFunction is a type alias for a function that receives an Engine and
+// returns an int.
 type ScriptFunction func(*Engine) int
 
+// Create a new engine containing a new lua.LState.
 func NewEngine() *Engine {
 	return &Engine{
 		state: lua.NewState(),
 	}
 }
 
+// Close will perform a close on the Lua state.
 func (e *Engine) Close() {
 	e.state.Close()
 }
 
+// LoadFile runs the file through the Lua interpreter.
 func (e *Engine) LoadFile(fn string) error {
 	return e.state.DoFile(fn)
 }
 
+// LoadString runs the given string through the Lua interpreter.
 func (e *Engine) LoadString(src string) error {
 	return e.state.DoString(src)
 }
 
+// SetVal allows for setting global variables in the loaded code.
 func (e *Engine) SetVal(name string, val *Value) {
 	e.state.SetGlobal(name, val.lval)
 }
 
+// RegisterFunc registers a Go function (ScriptFunction) with the script.
+// Using this method makes Go functions accessible through Lua scripts.
 func (e *Engine) RegisterFunc(name string, fn ScriptFunction) {
 	e.state.SetGlobal(name, e.genScriptFunc(fn))
 }
 
-func (e *Engine) genScriptFunc(fn ScriptFunction) lua.LValue {
-	sfn := func(l *lua.LState) int {
-		e := &Engine{state: l}
-
-		return fn(e)
-	}
-
-	return e.state.NewFunction(sfn)
-}
-
+// PopArg returns the top value on the Lua stack.
+// This method is used to get arguments given to a Go function from a Lua script.
+// This method will return a Value pointer that can then be converted into
+// an appropriate type.
 func (e *Engine) PopArg() *Value {
 	lv := e.state.Get(-1)
 	e.state.Pop(1)
@@ -51,10 +57,17 @@ func (e *Engine) PopArg() *Value {
 	return newValue(lv)
 }
 
+// PushRet pushes the given Value onto the Lua stack.
+// Use this method when 'returning' values from a Go function called from a
+// Lua script.
 func (e *Engine) PushRet(v *Value) {
 	e.state.Push(v.lval)
 }
 
+// Call allows for calling a method by name.
+// The second parameter is the number of return values the function being
+// called should return. These values will be returned in a slice of Value
+// pointers.
 func (e *Engine) Call(name string, retCount int, params ...*Value) ([]*Value, error) {
 	luaParams := make([]lua.LValue, len(params))
 	for i, v := range params {
@@ -80,6 +93,19 @@ func (e *Engine) Call(name string, retCount int, params ...*Value) ([]*Value, er
 	return retVals, nil
 }
 
+// LuaTable creates and returns a new LuaTable.
 func (e *Engine) LuaTable() *Value {
 	return newValue(e.state.NewTable())
+}
+
+// genScriptFunc will wrap a ScriptFunction with a function that gopher-lua
+// expects to see when calling method from Lua.
+func (e *Engine) genScriptFunc(fn ScriptFunction) lua.LValue {
+	sfn := func(l *lua.LState) int {
+		e := &Engine{state: l}
+
+		return fn(e)
+	}
+
+	return e.state.NewFunction(sfn)
 }
--- value.go
+++ value.go
@@ -9,6 +9,8 @@ import (
 // Provides information about failed Value typecasts.
 type ValueError string
 
+// newValueError creates a new error explaining failure from a given type to an
+// actual type.
 func newValueError(exp string, v *Value) ValueError {
 	return ValueError(fmt.Sprintf("expected %s, found \"%s\"", exp, v.lval.Type()))
 }
@@ -18,47 +20,211 @@ func (v ValueError) Error() string {
 	return string(v)
 }
 
-// Utility wrapper for lua.LValue that provies conveinient methods for casting.
+// Value is a utility wrapper for lua.LValue that provies conveinient methods
+// for casting.
 type Value struct {
 	lval lua.LValue
 }
 
+// newValue constructs a new value from an LValue.
 func newValue(val lua.LValue) *Value {
 	return &Value{
 		lval: val,
 	}
 }
 
-// Returns the LValue as a Go string
-func (v *Value) AsString() (string, error) {
-	if str, ok := v.lval.(lua.LString); ok {
-		return string(str), nil
-	} else {
-		return "", newValueError("string", v)
-	}
+// String makes Value conform to Stringer
+func (v *Value) String() string {
+	return v.lval.String()
 }
 
-// Returns the LValue as a Go
-func (v *Value) AsFloat() (float64, error) {
-	if f, ok := v.lval.(lua.LNumber); ok {
-		return float64(f), nil
-	} else {
-		return 0.0, newValueError("number", v)
-	}
+// AsString returns the LValue as a Go string
+func (v *Value) AsString() string {
+	return lua.LVAsString(v.lval)
 }
 
+// AsFloat returns the LValue as a Go float64.
+// This method will try to convert the Lua value to a number if possible, if
+// not then LuaNumber(0) is returned.
+func (v *Value) AsFloat() float64 {
+	return float64(lua.LVAsNumber(v.lval))
+}
+
+// AsNumber is an alias for AsFloat (Lua calls them "numbers")
 func (v *Value) AsNumber() (float64, error) {
 	return v.AsFloat()
 }
 
-func (v *Value) AsBool() (bool, error) {
-	if b, ok := v.lval.(lua.LBool); ok {
-		return b == lua.LTrue, nil
-	} else {
-		return false, newValueError("bool", v)
+// AsBool returns the Lua boolean representation for an object (this works for
+// non bool Values)
+func (v *Value) AsBool() bool {
+	return lua.LVAsBool(v.lval)
+}
+
+// IsNil will only return true if the Value wraps LNil.
+func (v *Value) IsNil() bool {
+	return v.lval.Type() == lua.LTNil
+}
+
+// IsFalase is similar to AsBool except it returns if the Lua value would be
+// considered false in Lua.
+func (v *Value) IsFalse() bool {
+	return lua.LVIsFalse(v.lval)
+}
+
+// The following methods allow LTable values to be modified through Go.
+
+// isTable returns a bool if the Value is an LTable.
+func (v *Value) isTable() bool {
+	return v.lval.Type() == lua.LTTable
+}
+
+// asTable converts the Value into an LTable.
+func (v *Value) asTable() (t *lua.LTable) {
+	t, _ = v.lval.(*lua.LTable)
+
+	return
+}
+
+// TableAppend maps to lua.LTable.Append
+func (v *Value) TableAppend(value *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		t.Append(value.lval)
 	}
 }
 
-func (v *Value) IsNil() bool {
-	return v.lval.Type() == lua.LTNil
+// TableForEach maps to lua.LTable.ForEach
+func (v *Value) TableForEach(cb func(*Value, *Value)) {
+	if v.isTable() {
+		actualCb := func(key lua.LValue, val lua.LValue) {
+			cb(newValue(key), newValue(val))
+		}
+		t := v.asTable()
+		t.ForEach(actualCb)
+	}
+}
+
+// TableInsert maps to lua.LTable.Insert
+func (v *Value) TableInsert(i int, value *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		t.Insert(i, value.lval)
+	}
+}
+
+// TableLen maps to lua.LTable.Len
+func (v *Value) TableLen() int {
+	if v.isTable() {
+		t := v.asTable()
+
+		return t.Len()
+	}
+
+	return -1
+}
+
+// TableMaxN maps to lua.LTable.MaxN
+func (v *Value) TableMaxN() int {
+	if v.isTable() {
+		t := v.asTable()
+
+		return t.MaxN()
+	}
+
+	return 0
+}
+
+// TableNext maps to lua.LTable.Next
+func (v *Value) TableNext(key *Value) (*Value, *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		v1, v2 := t.Next(key.lval)
+
+		return newValue(v1), newValue(v2)
+	}
+
+	return LuaNil(), LuaNil()
+}
+
+// TableRawGet maps to lua.LTable.RawGet
+func (v *Value) TableRawGet(key *Value) *Value {
+	if v.isTable() {
+		t := v.asTable()
+		ret := t.RawGet(key.lval)
+
+		return newValue(ret)
+	}
+
+	return LuaNil()
+}
+
+// TableRawGetH maps to lua.LTable.RawGetH
+func (v *Value) TableRawGetH(key *Value) *Value {
+	if v.isTable() {
+		t := v.asTable()
+		ret := t.RawGetH(key.lval)
+
+		return newValue(ret)
+	}
+
+	return LuaNil()
+}
+
+// TableRawGetInt maps to lua.LTable.RawGetInt
+func (v *Value) TableRawGetInt(key int) *Value {
+	if v.isTable() {
+		t := v.asTable()
+		ret := t.RawGetInt(key)
+
+		return newValue(ret)
+	}
+
+	return LuaNil()
+}
+
+// TableRawSet maps to lua.LTable.RawSet
+func (v *Value) TableRawSet(key *Value, value *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		t.RawSet(key.lval, value.lval)
+	}
+}
+
+// TableRawSetH maps to lua.LTable.RawSetH
+func (v *Value) TableRawSetH(key *Value, value *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		t.RawSetH(key.lval, value.lval)
+	}
+}
+
+// TableRawSetInt maps to lua.LTable.RawSetInt
+func (v *Value) TableRawSetInt(key int, value *Value) {
+	if v.isTable() {
+		t := v.asTable()
+		t.RawSetInt(key, value.lval)
+	}
+}
+
+// TableRemove maps to lua.LTable.Remove
+func (v *Value) TableRemove(pos int) *Value {
+	if v.isTable() {
+		t := v.asTable()
+		ret := t.Remove(pos)
+
+		return newValue(ret)
+	}
+
+	return LuaNil()
+}
+
+// The following provide LFunction methods on Value
+
+func (v *Value) FuncLocalName(regno, pc int) (string, bool) {
+	if f, ok := v.lval.(lua.LFunction); ok {
+		return f.LocalName(regno, pc)
+	} else {
+		return "", false
+	}
 }
