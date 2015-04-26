e1f1441ef942d83bc99b8ab9a2fbb116c3df6819
Brandon Buck <lordizuriel@gmail.com>
2015-04-26 01:40:38 -0500

    Add more examples, with tags.

--- examples_test.go
+++ examples_test.go
@@ -17,3 +17,15 @@ func ExampleEngine() {
 	n := ret[0].AsNumber()
 	fmt.Println(n) // 20.0000000
 }
+
+func ExampleCall_simple() {
+	e.Call("some_method", 0)
+}
+
+func ExampleCall_multiple_returns() {
+	ret, _ := e.Call("swap_these_numbers", 2, LuaNumber(10), LuaNumber(20))
+
+	a, b := ret[0].AsNumber(), ret[1].AsNumber()
+	fmt.Println(a) // 20.0000000
+	fmt.Println(b) // 10.0000000
+}
