d85d42aaf2003f3e681e53a2e14963817562de1a
Brandon Buck <lordizuriel@gmail.com>
2015-04-25 23:09:49 -0500

    Remaining documentation.

--- lua_val_wrappers.go
+++ lua_val_wrappers.go
@@ -2,18 +2,22 @@ package engine
 
 import "github.com/yuin/gopher-lua"
 
+// LuaNumber converts a float to a Value representing a number in Lua.
 func LuaNumber(f float64) *Value {
 	return newValue(lua.LNumber(f))
 }
 
+// Lua string returns a Value representing a string in Lua.
 func LuaString(s string) *Value {
 	return newValue(lua.LString(s))
 }
 
+// LuaNil returns the Nil value for Lua.
 func LuaNil() *Value {
 	return newValue(lua.LNil)
 }
 
+// LuaBool converts a Go bool into a Lua bool type.
 func LuaBool(b bool) *Value {
 	if b {
 		return newValue(lua.LTrue)
